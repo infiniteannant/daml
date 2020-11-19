a2d643ad71ea9fb567b429f032234e940fadf388
Andreas Herrmann <andreas.herrmann@tweag.io>
2020-11-19 17:57:46 +0100

    Store refresh token in trigger DB

--- triggers/service/src/main/scala/com/digitalasset/daml/lf/engine/trigger/Server.scala
+++ triggers/service/src/main/scala/com/digitalasset/daml/lf/engine/trigger/Server.scala
@@ -34,7 +34,7 @@ import com.daml.lf.data.Ref.{Identifier, PackageId}
 import com.daml.lf.engine._
 import com.daml.lf.engine.trigger.Request.StartParams
 import com.daml.lf.engine.trigger.Response._
-import com.daml.lf.engine.trigger.Tagged.AccessToken
+import com.daml.lf.engine.trigger.Tagged.{AccessToken, RefreshToken}
 import com.daml.lf.engine.trigger.TriggerRunner._
 import com.daml.lf.engine.trigger.dao._
 import com.daml.oauth.middleware.Request.Claims
@@ -143,10 +143,16 @@ class Server(
   // Note that this does not yet start the trigger.
   private def addNewTrigger(
       config: TriggerConfig,
-      token: Option[AccessToken]
+      auth: Option[Authorization],
   )(implicit ec: ExecutionContext): Future[Either[String, (Trigger, RunningTrigger)]] = {
     val runningTrigger =
-      RunningTrigger(config.instance, config.name, config.party, config.applicationId, token)
+      RunningTrigger(
+        config.instance,
+        config.name,
+        config.party,
+        config.applicationId,
+        auth.map(_.accessToken),
+        auth.flatMap(_.refreshToken))
     // Validate trigger id before persisting to DB
     Trigger.fromIdentifier(compiledPackages, runningTrigger.triggerName) match {
       case Left(value) => Future.successful(Left(value))
@@ -164,7 +170,7 @@ class Server(
             runningTrigger.triggerInstance,
             runningTrigger.triggerParty,
             runningTrigger.triggerApplicationId,
-            runningTrigger.triggerToken,
+            runningTrigger.triggerAccessToken,
             compiledPackages,
             trigger,
             ledgerConfig,
@@ -223,6 +229,8 @@ class Server(
   // TODO[AH] Make sure this is bounded in size.
   private val authCallbacks: TrieMap[UUID, Route] = TrieMap()
 
+  case class Authorization(accessToken: AccessToken, refreshToken: Option[RefreshToken])
+
   // This directive requires authorization for the given claims via the auth middleware, if configured.
   // If no auth middleware is configured, then the request will proceed without attempting authorization.
   //
@@ -232,14 +240,14 @@ class Server(
   // to proceed once the login flow completed and authentication succeeded.
   private def authorize(claims: AuthRequest.Claims)(
       implicit ec: ExecutionContext,
-      system: ActorSystem): Directive1[Option[AccessToken]] =
+      system: ActorSystem): Directive1[Option[Authorization]] =
     authConfig match {
       case NoAuth => provide(None)
       case AuthMiddleware(authUri) =>
         // Attempt to obtain the access token from the middleware's /auth endpoint.
         // Forwards the current request's cookies.
         // Fails if the response is not OK or Unauthorized.
-        def auth: Directive1[Option[AccessToken]] = {
+        def auth: Directive1[Option[Authorization]] = {
           val uri = authUri
             .withPath(Path./("auth"))
             .withQuery(AuthRequest.Auth(claims).toQuery)
@@ -248,7 +256,10 @@ class Server(
             onSuccess(Http().singleRequest(HttpRequest(uri = uri, headers = cookies))).flatMap {
               case HttpResponse(StatusCodes.OK, _, entity, _) =>
                 onSuccess(Unmarshal(entity).to[AuthResponse.Authorize]).map { auth =>
-                  Some(AccessToken(auth.accessToken)): Option[AccessToken]
+                  Some(
+                    Authorization(
+                      AccessToken(auth.accessToken),
+                      RefreshToken.subst(auth.refreshToken))): Option[Authorization]
                 }
               case HttpResponse(StatusCodes.Unauthorized, _, _, _) =>
                 provide(None)
@@ -266,7 +277,7 @@ class Server(
         Directive { inner =>
           auth {
             // Authorization successful - pass token to continuation
-            case Some(token) => inner(Tuple1(Some(token)))
+            case Some(authorization) => inner(Tuple1(Some(authorization)))
             // Authorization failed - login and retry on callback request.
             case None => { ctx =>
               val requestId = UUID.randomUUID()
@@ -278,10 +289,10 @@ class Server(
                       // TODO[AH] Add WWW-Authenticate header
                       complete(errorResponse(StatusCodes.Unauthorized))
                     }
-                    case Some(token) =>
+                    case Some(authorization) =>
                       // Authorization successful after login - use old request context and pass token to continuation.
                       mapRequestContext(_ => ctx) {
-                        inner(Tuple1(Some(token)))
+                        inner(Tuple1(Some(authorization)))
                       }
                   }
                 }
@@ -305,7 +316,7 @@ class Server(
   // If the trigger does not exist, then the request will also proceed without attempting authorization.
   private def authorizeForTrigger(uuid: UUID, readOnly: Boolean = false)(
       implicit ec: ExecutionContext,
-      system: ActorSystem): Directive1[Option[AccessToken]] =
+      system: ActorSystem): Directive1[Option[Authorization]] =
     authConfig match {
       case NoAuth => provide(None)
       case AuthMiddleware(_) =>
@@ -356,9 +367,9 @@ class Server(
                       applicationId = Some(config.applicationId))
                   // TODO[AH] Why do we need to pass ec, system explicitly?
                   authorize(claims)(ec, system) {
-                    token =>
+                    auth =>
                       val instOrErr: Future[Either[String, JsValue]] =
-                        addNewTrigger(config, token)
+                        addNewTrigger(config, auth)
                           .map(_.map {
                             case (trigger, runningTrigger) => startTrigger(trigger, runningTrigger)
                           })
